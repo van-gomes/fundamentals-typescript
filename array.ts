712cd7071cc08427815beffabd2c8dd17cca70bd
van-gomes <nninha.gomes@gmail.com>
2024-06-20 17:31:05 -0300

    Study forEach and map

--- array.ts
+++ array.ts
@@ -14,3 +14,24 @@ console.log("Array com novo elemento: ", numberArray);
 
 numberArray.pop();
 console.log("Array com o último elemento removido: ", numberArray);
+
+/*find compara elemento por elemento, quando
+encontrar o que atende a condição, devolve o resultado */
+const buscaNumero = numberArray.find(num => num === 4);
+console.log(buscaNumero);
+
+stringArray.forEach(str => console.log(str));
+
+numberArray.forEach(num => {
+    if(num > 2 && num % 2 === 0){
+        console.log(num * 2);
+    }
+});
+
+numberArray.map(num => console.log(num));
+
+numberArray.map(num => {
+    if(num > 2){
+        console.log(num * 2);
+    }
+});

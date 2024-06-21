fd8f3965ef3477146246a9078106d283197b908d
van-gomes <nninha.gomes@gmail.com>
2024-06-21 19:00:36 -0300

    Study POO inheritance

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
-class Acount {
+abstract class Acount {
     name: string
     acountNumber: number
+    balance: number = 0
 
     constructor(name: string, acountNumber: number) {
         this.name = name
@@ -18,10 +19,29 @@ class Acount {
     requestLoan = () => {
         console.log('Você solicitou empréstimo')
     }
+
+    getBalance = () => {
+        console.log(this.balance);
+    }
 }
 
+class PeopleAcount extends Acount {
+    doc_id: number
+
+    constructor(doc_id: number, name: string, acountNumber: number) {
+        super(name, acountNumber)
+        this.doc_id = doc_id
+    }
+}
+
+/* 
 const acount: Acount = new Acount('Diego', 1);
 console.log(acount);
 
 const newAcount: Acount = new Acount('Rafael', 2);
 newAcount.requestLoan();
+
+const acountAdmin: Admin = new Admin('Rafael', 2);
+console.log(acountAdmin);
+*/
+
